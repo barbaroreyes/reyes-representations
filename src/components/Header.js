3dd011b3b572d41d2b8943facb30f1e4ea901a20
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-09 15:24:43 -0500

    update

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Header = () => {
+    return (
+        <header className='dad'>
+            Header
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main className='dad'>
+       Main
+    </main>
+  )
+}
+
+export default Main
