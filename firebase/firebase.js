b979ab807c1462cc9769cd66b583d45d209e95dd
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-09 14:24:26 -0500

    update tools

---
+++ firebase/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+var firebaseConfig = {
+    apiKey: "AIzaSyCalkH2aKSclIWeCxpmY9vB84Ps9BeL-Ak",
+    authDomain: "app-agency-de167.firebaseapp.com",
+    projectId: "app-agency-de167",
+    storageBucket: "app-agency-de167.appspot.com",
+    messagingSenderId: "833454734846",
+    appId: "1:833454734846:web:c7adb1856fea2067fdd4af"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
