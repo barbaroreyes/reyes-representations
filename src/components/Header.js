0b222a9c992175241d05ae701ef7432c2f4f7312
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-10 21:46:49 -0500

    update-nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Nav from './nav'
+import Logo from './logo'
 
  const Header = () => {
     return (
-        <header className='dad'>
-            Header
+        <header className='dad box1'> 
+            <Nav/>
+            <Logo/>
         </header>
     )
 }
